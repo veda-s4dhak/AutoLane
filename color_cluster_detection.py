94b646e6c0ca292712de28f0af6d7848f939c086
Anish Agarwal <a22agarw@outlook.com>
2017-09-10 18:59:54 -0700

    Update color_cluster_detection.py
    Updated print_color_pixels
    Updated find_red_pixels
    Updated find_clusters
    Updated is_pixel_adjacent
    Added filter_cluster_by_size
    Added find_upper_left_pixel
    Cleaned up main function

--- color_cluster_detection.py
+++ color_cluster_detection.py
@@ -24,7 +24,7 @@ def print_color_pixels(im):
     x_max, y_max = rgb_im.size
 
     for x in range(1, x_max):
-        for j in range(1, y_max):
+        for y in range(1, y_max):
             print( rgb_im.getpixel((x,y)) )
 
 #=======================================================================================================================
@@ -69,15 +69,32 @@ def find_clusters(x_coordinates,y_coordinates):
         while (i < len(x_coordinates)):
             for j in range(0,len(cluster_x)):
 
-                #print("i: {} | j: {}".format(i,j))
+                if (i >= len(x_coordinates)): #Need to break because while loop condition is not checked in for loop
+                    break
 
                 if ( is_pixel_adjacent( x_coordinates[i],y_coordinates[i],cluster_x[j],cluster_y[j] ) == True ):
+                    # print("Condition checked: ( abs({}-{}) <= 10 ) and ( abs({}-{}) <= 10 ) == {}".format(
+                    #     x_coordinates[i], cluster_x[j],y_coordinates[i], cluster_y[j],
+                    #       is_pixel_adjacent(x_coordinates[i], y_coordinates[i], cluster_x[j], cluster_y[j]) ))
+
                     cluster_x.append(x_coordinates[i])
                     cluster_y.append(y_coordinates[i])
                     del x_coordinates[i]
                     del y_coordinates[i]
+                    i = 0
+                else:
+                    # print("Condition checked: ( abs({}-{}) <= 10 ) and ( abs({}-{}) <= 10 ) == {}".format(
+                    #     x_coordinates[i], cluster_x[j], y_coordinates[i], cluster_y[j],
+                    #     is_pixel_adjacent(x_coordinates[i], y_coordinates[i], cluster_x[j], cluster_y[j])))
 
-            i += 1
+                    i += 1
+
+                # print("i: {}".format(i))
+                # print("Current X Cluster: {}".format(cluster_x))
+                # print("Current Y Cluster: {}".format(cluster_y))
+                # print("Current X Coord: {}".format(x_coordinates))
+                # print("Current Y Coord: {}".format(y_coordinates))
+                # input("Press any key to continue...")
 
         cluster_x_list.append(cluster_x)
         cluster_y_list.append(cluster_y)
@@ -96,7 +113,7 @@ def is_pixel_adjacent(x1,y1,x2,y2):
 
     adjacent = False
 
-    if ( ( abs(x1 - x2) <= 1) and ( abs(y1 - y2) <= 1) ):
+    if ( ( abs(x1 - x2) <= 40) and ( abs(y1 - y2) <= 20) ):
         adjacent = True
 
     return adjacent
@@ -106,14 +123,32 @@ def is_pixel_adjacent(x1,y1,x2,y2):
 def output_clusters_to_image(im,x_clusters,y_clusters):
     pixel_map = im.load()
 
+    color_cnt = 0;
+    color = 255;
+
     for cluster_num in range( 0,len(x_clusters) ):
-        print("Current Cluster Length: {}".format( len(x_clusters[cluster_num]) ))
 
-        if ( len(x_clusters[cluster_num]) > 25 ):
-            for coord in range( 0,len(x_clusters[cluster_num]) ):
-                pixel_map[ x_clusters[cluster_num][coord],y_clusters[cluster_num][coord] ] = (255, 255, 255, 255)
+        for coord in range( 0,len(x_clusters[cluster_num]) ):
+            if (color_cnt == 0):
+                pixel_map[ x_clusters[cluster_num][coord],y_clusters[cluster_num][coord] ] = (color, 255, 255, 255)
+            if (color_cnt == 1):
+                pixel_map[ x_clusters[cluster_num][coord],y_clusters[cluster_num][coord] ] = (255, color, 255, 255)
+            if (color_cnt == 2):
+                pixel_map[ x_clusters[cluster_num][coord],y_clusters[cluster_num][coord] ] = (255, 255, color, 255)
 
-        x_max,y_max = im.size
+        # Changing colors for each cluster
+        color_cnt+=1
+
+        if (color_cnt >= 3):
+            color_cnt = 0;
+
+        color = color-50
+        if (color <= 0):
+            color = 255;
+
+
+    # Creating the new image
+    x_max,y_max = im.size
 
     new_img = PIL_image.new(im.mode, im.size)
     new_pixel_map = new_img.load()
@@ -126,6 +161,41 @@ def output_clusters_to_image(im,x_clusters,y_clusters):
     new_img.save(r'C:\Users\Veda Sadhak\Google Drive\A Different Time\AI Projects\Minion_Detection\Health_Bar\hb_detect.png')
     new_img.close()
 
+#=======================================================================================================================
+
+def filter_cluster_by_size(x_clusters,y_clusters,size = 50):
+
+
+    for cluster_num in range(0,len(x_clusters)):
+
+        if len(x_clusters[cluster_num]) < size:
+            del x_clusters[cluster_num]
+
+        if len(y_clusters[cluster_num]) < size:
+            del y_clusters[cluster_num]
+
+        if (cluster_num >= len(x_clusters)):
+            break
+
+    return x_clusters , y_clusters;
+    
+#=======================================================================================================================
+
+def find_upper_left_pixel(x_cluster,y_cluster):
+    lowest_x = 9999
+    lowest_y = 9999
+
+    for i in range(0,len(y_cluster)):
+        if y_cluster[i] < lowest_y:
+            lowest_y = y_cluster[i]
+
+    for i in range(0,len(x_cluster)):
+        if y_cluster[i] == lowest_y:
+            if x_cluster[i] < lowest_x:
+                lowest_x = x_cluster[i]
+
+    return lowest_x , lowest_y    
+   
 #=======================================================================================================================
 
 def get_minion_area(im, cluster_x, cluster_y, size = 75):
@@ -146,6 +216,7 @@ def get_minion_area(im, cluster_x, cluster_y, size = 75):
 
 if __name__ == '__main__':
     #get_screenshots(5,r'C:\\Users\\Veda Sadhak\\Google Drive\\A Different Time\\AI Projects\\Minion_Detection\\Image_Set\\',r'league')
+    
     im_number = 104
     create_dir(im_number)
     
@@ -162,6 +233,8 @@ if __name__ == '__main__':
     print("Done Finding Red Pixels...")
     print()
 
+   #========================================================================
+
     print("Finding Clusters...")
 
     x_clusters , y_clusters = find_clusters(r_pix_x_coord,r_pix_y_coord)
@@ -173,10 +246,37 @@ if __name__ == '__main__':
 
     print("Done Finding Clusters...")
     print()
+    
+    # ========================================================================
+
+    print("Filtering Clusters...")
+
+    fil_x_clusters, fil_y_clusters = filter_cluster_by_size(x_clusters, y_clusters,50)
+
+    print("Filtered X Clusters: {}".format(fil_x_clusters))
+    print("Size Filtered X Cluster: {}".format(len(fil_x_clusters)))
+    print("Filtered Y Clusters: {}".format(fil_y_clusters))
+    print("Size Filtered Y Cluster: {}".format(len(fil_y_clusters)))
+
+    print("Done Filtering Clusters...")
+    print()
+                        
+    # ========================================================================
+
+    print("Finding upper left pixel of each cluster...")
+
+    for cluster_num in range(0,len(fil_x_clusters)):
+        upper_left_pix_x , upper_left_pix_y = find_upper_left_pixel(fil_x_clusters[cluster_num],fil_y_clusters[cluster_num])
+        print("Cluster Num: {} | X: {} | Y: {}".format( cluster_num,upper_left_pix_x,upper_left_pix_y ))
+
+    print("Done finding upper left pixel of each cluster...")
+    print()
+
+    # ========================================================================
 
     print("Creating New Image...")
 
-    output_clusters_to_image(im, x_clusters, y_clusters)
+    output_clusters_to_image(im, fil_x_clusters, fil_y_clusters)
 
     print("Done Creating New Image...")
     print()
